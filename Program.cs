2e0171e6c87172bc62826fda8a8da44c67b2073e
mathycreater <46695762+mathycreater@users.noreply.github.com>
2020-07-28 19:11:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace classe_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo X, Y;
+            X = new Triangulo();
+            Y = new Triangulo();
+            
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            X.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            Y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = X.Area();
+            double areaY = Y.Area();
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace classe_triangulo
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+        }
+    }
+    
+}
