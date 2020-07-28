7d336606b4eef96aa687a4c1342a5178f882823c
mathycreater <46695762+mathycreater@users.noreply.github.com>
2020-07-28 19:16:01 -0300

    Update Triangulo.cs

--- Triangulo.cs
+++ Triangulo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+// Calcular area de um triangulo usando POO. (Acima: Código para a classe. Abaixo: Código para o console)
+using System;
 
 namespace classe_triangulo
 {
@@ -16,4 +17,46 @@ namespace classe_triangulo
         }
     }
     
+}
+using System;
+using System.Globalization;
+
+namespace classe_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo X, Y;
+            X = new Triangulo();
+            Y = new Triangulo();
+            
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            X.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            Y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = X.Area();
+            double areaY = Y.Area();
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+
+    }
 }
